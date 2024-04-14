a8f06872fba9ea2f50e663a6a956594d42be1980
Marc <marc@wynglobalmedia.com>
2024-04-15 00:47:58 +0800

    refactor: update admin meta box voting title

--- wp-app/wp-content/plugins/article-voting/templates/admin/voting-results-meta-box.php
+++ wp-app/wp-content/plugins/article-voting/templates/admin/voting-results-meta-box.php
@@ -1,5 +1,5 @@
 <div class="article-voting-results">
     <p><?php _e('Voting Results:', 'article-voting'); ?></p>
-    <p><?php printf(__('Positive: %s%%', 'article-voting'), $voting_results['positive_percentage']); ?></p>
-    <p><?php printf(__('Negative: %s%%', 'article-voting'), $voting_results['negative_percentage']); ?></p>
+    <p><?php printf(__('Yes: %s%%', 'article-voting'), $voting_results['positive_percentage']); ?></p>
+    <p><?php printf(__('No: %s%%', 'article-voting'), $voting_results['negative_percentage']); ?></p>
 </div>
